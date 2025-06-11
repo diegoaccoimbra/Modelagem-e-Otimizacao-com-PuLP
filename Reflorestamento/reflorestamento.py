a69945026a608736a1c056899d69e155d2f202f0
Diego Coimbra <93612871+diegoaccoimbra@users.noreply.github.com>
2025-06-11 20:26:29 -0300

    Status corrigido

--- Reflorestamento/reflorestamento.py
+++ Reflorestamento/reflorestamento.py
@@ -14,10 +14,10 @@ producao_esperada = {
 
 # Produção mínima para cada árvore
 producao_minima = {
-    "Pinus": 225 * 1000,
-    "Carvalho": 9 * 1000,
-    "Nogueira": 4.8 * 1000,
-    "Araucacia": 3.5 * 1000
+    "Pinus": 225 * 10,
+    "Carvalho": 9 * 10,
+    "Nogueira": 4.8 * 10,
+    "Araucacia": 3.5 * 10
 }
 
 # Área disponível para plantio em cada cidade
@@ -48,21 +48,21 @@ lista_cidades = list(area_disponivel.keys())
 lista_arvores = list(producao_minima.keys())
 
 # Variáveis de decisão (áreas de plantio de cada árvore em cada cidade)
-plantio = {(cidade, arvore): pulp.LpVariable(f"area {cidade}{arvore}", lowBound = 0, cat = 'Continuous') for cidade in lista_cidades for arvore in lista_arvores}
+plantio = {(cidade, arvore): LpVariable(f"area {cidade}{arvore}", lowBound = 0, cat = 'Continuous') for cidade in lista_cidades for arvore in lista_arvores}
 
 # Função objetivo (maximizar a renda)
-prob += pulp.lpSum(renda_esperada[cidade][arvore] * plantio[cidade][arvore] for cidade in lista_cidades for arvore in lista_arvores),  "Renda_Total"
+prob += lpSum(renda_esperada[cidade][arvore] * plantio[(cidade, arvore)] for cidade in lista_cidades for arvore in lista_arvores), "Renda_Total"
 
 # Restrição da área disponível para plantio
 for cidade in lista_cidades:
     prob += (
-        pulp.lpSum(plantio[cidade][arvore] for arvore in lista_arvores) <= area_disponivel[cidade], f"Area_Disponivel_{cidade}"
+        lpSum(plantio[(cidade, arvore)] for arvore in lista_arvores) <= area_disponivel[cidade], f"Area_Disponivel_{cidade}"
     )
 
 # Restrição da produção mínima
 for arvore in lista_arvores:
     prob += (
-        pulp.lpSum(producao_esperada[cidade][arvore] * plantio[cidade][arvore] for cidade in lista_cidades) >= producao_minima[arvore], f"Producao_Mínima_{arvore}"
+        lpSum(producao_esperada[cidade][arvore] * plantio[(cidade, arvore)] for cidade in lista_cidades) >= producao_minima[arvore], f"Producao_Mínima_{arvore}"
     )
 
 # --------------------------
@@ -75,15 +75,22 @@ prob.solve()
 # Relatório
 # --------------------------
 
-print(f"Status da Solução: {LpStatus[prob.status]}\n")
+print(f"Status: {LpStatus[prob.status]}\n")
 
-if prob.status == LpStatus.Optimal:
-    print(f"Renda Total Máxima: {prob.objective.value:.2f} Unidades Monetárias\n")
-    print("Áreas de Plantio Ótimas por Cidade e Espécie:")
+# Encontrou uma solução ótima
+if prob.status == LpStatusOptimal:
+    print(f"Renda Total Máxima: R$ {prob.objective.value():.2f}\n")
+
+    print("Áreas de Plantio por Cidade e Árvore:\n")
     for cidade in lista_cidades:
+        print(f"{cidade}:")
         for arvore in lista_arvores:
-            valor_area = plantio[(cidade, arvore)].value
-            if valor_area > 0: # Exibir apenas as áreas que serão plantadas
-                print(f"  {cidade} - {arvore}: {valor_area:.2f} hectares")
+            valor_area = plantio[(cidade, arvore)].varValue
+            # Exibe apenas as áreas que serão plantadas maior que zero
+            #if valor_area > 0:
+            print(f"- {arvore}: {valor_area:.2f} hectares")
+        print("\n")
+
+# Caso não encontre uma solução ótima
 else:
-    print("Não foi possível encontrar uma solução ótima.")
+    print("Não foi encontrada uma solução ótima")
